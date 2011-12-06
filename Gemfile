04163af45586457ae9f7e3c2efc026ce34d4ece3
Chris Game <chris.game@robbiebobbins.com>
2011-12-06 23:03:45 +0000

    CG: Reverted to running locally and not using heroku. New project created for that

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source "http://rubygems.org"
-
+gem "ruby-debug-base19x"
+gem "ruby-debug19"
+gem "ruby-debug-ide19"
 gem "cucumber"
 gem "rspec"
 
 gem "sinatra"
 gem "right_aws"
 gem 'haml'
-gem 'heroku'
--- lib/app.rb
+++ lib/app.rb
@@ -1,15 +1,19 @@
 require 'sinatra'
+require 'right_aws'
 require 'haml'
 
 class S3Test < Sinatra::Base
+  AWS_ACCESS_KEY = 'AKIAI3HDDHE7NJ2HWRQA'
+  AWS_SECRET_ACCESS_KEY = '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'
+  AWS_BUCKET = 'robbiebobbins'
 
   put '/' do
-    puts 'Hit put on root'
-    AWS::S3::S3Object.store(params['S3Test/test.jpg'], request.body, AWS_CONFIG['robbiebobbins'])
+    s3 = RightAws::S3.new(AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY)
+    bobbins_bucket = s3.bucket(AWS_BUCKET)
+    bobbins_bucket.put('S3Test/test.jpg', request.body)
   end
 
   get '/' do
-    puts 'Hit get on root'
     page = "<img src='http://s3.amazonaws.com/robbiebobbins/S3Test/test.jpg'/>"
     haml page
   end
@@ -20,5 +24,4 @@ class S3Test < Sinatra::Base
     bobbins_bucket = s3.bucket(AWS_BUCKET)
     "Buckets on S3: #{bucket_names.join(', ')} Keys in robbiebobbins #{bobbins_bucket.keys.join(', ')}"
   end
-
 end
