9b27b876b25f36ecb81d6c7a8c0358f6158e8b0b
Chris Game <chris.game@robbiebobbins.com>
2011-12-06 21:05:17 +0000

    CG: Setup for heroku

--- lib/app.rb
+++ lib/app.rb
@@ -1,24 +1,11 @@
 require 'sinatra'
-require 'right_aws'
 require 'haml'
 
 class S3Test < Sinatra::Base
-  puts 'Setting up access key'
-  AWS_ACCESS_KEY = 'AKIAI3HDDHE7NJ2HWRQA'
-  puts 'Setting up secret access key'
-  AWS_SECRET_ACCESS_KEY = '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'
-  puts 'Setting up bucket'
-  AWS_BUCKET = 'robbiebobbins'
-  puts 'AWS setup complete'
 
   put '/' do
     puts 'Hit put on root'
-    puts 'About to new bucket'
-    s3 = RightAws::S3.new(AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY)
-    puts 'assigning bobbins bucket'
-    bobbins_bucket = s3.bucket(AWS_BUCKET)
-    puts 'about to make put request'
-    bobbins_bucket.put('S3Test/test.jpg', request.body, 'public-read')
+    AWS::S3::S3Object.store(params['S3Test/test.jpg'], request.body, AWS_CONFIG['robbiebobbins'])
   end
 
   get '/' do
